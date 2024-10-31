02356acc8914703f8f99c8cfefdc9fef9889e2c2
Ere Maijala <ere.maijala@helsinki.fi>
2024-10-31 08:55:01 +0200

    Fix test download paths and tweak scripts.

--- src/BeSimple/SoapClient/Tests/bin/axis.sh
+++ src/BeSimple/SoapClient/Tests/bin/axis.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
@@ -8,23 +8,26 @@ ZIP_AXIS=axis2-$VERSION_AXIS-bin.zip
 if [[ "$VERSION_AXIS" == "1.5.1" ]]; then
     PATH_AXIS=https://archive.apache.org/dist/ws/axis2/1_5_1/axis2-1.5.1-bin.zip
 elif [[ "$VERSION_AXIS" > "1.5.1" ]]; then
-    PATH_AXIS=http://archive.apache.org/dist/axis/axis2/java/core/$VERSION_AXIS/$ZIP_AXIS
+    PATH_AXIS=https://archive.apache.org/dist/axis/axis2/java/core/$VERSION_AXIS/$ZIP_AXIS
 else
-    PATH_AXIS=http://archive.apache.org/dist/ws/axis2/${VERSION_AXIS//./_}/$ZIP_AXIS
+    PATH_AXIS=https://archive.apache.org/dist/ws/axis2/${VERSION_AXIS//./_}/$ZIP_AXIS
 fi
 
 if [ ! -f "$DIR/$ZIP_AXIS" ]; then
+    echo "Downloading Axis"
     curl -O -s $PATH_AXIS
 fi
 
-VERSION_RAMPART=1.5
+VERSION_RAMPART=1.5.1
 ZIP_RAMPART=rampart-dist-$VERSION_RAMPART-bin.zip
-PATH_RAMPART=http://archive.apache.org/dist/axis/axis2/java/rampart/$VERSION_RAMPART/$ZIP_RAMPART
+PATH_RAMPART=https://archive.apache.org/dist/axis/axis2/java/rampart/$VERSION_RAMPART/$ZIP_RAMPART
 
 if [ ! -f "$DIR/$ZIP_RAMPART" ]; then
+    echo "Downloading Rampart"
     curl -O -s $PATH_RAMPART
 fi
 
+echo "Extracting packages"
 unzip -o -qq "$DIR/$ZIP_AXIS"
 
 AXIS_DIR=$DIR/axis2-$VERSION_AXIS
@@ -34,13 +37,22 @@ unzip -o -qq -j "$DIR/$ZIP_RAMPART" '*/modules/*.mar' -d $AXIS_DIR/repository/mo
 
 cp -r $DIR/../AxisInterop/axis_services/* $AXIS_DIR/repository/services
 
-$AXIS_DIR/bin/axis2server.sh&
+$AXIS_DIR/bin/axis2server.sh &
+AXIS2_PID=$!
+echo "Started process $AXIS2_PID"
 
 echo "Waiting until Axis is ready on port 8080"
+COUNT=1
 while [[ -z `curl -s 'http://localhost:8080/axis2/services/' ` ]]
 do
     echo -n "."
     sleep 1
+    COUNT=$COUNT+1
+    if (( "$COUNT" > 10 ))
+    then
+        echo "Timed out waiting for Axis to start up"
+        exit 1
+    fi
 done
 
-echo "Axis is up"
+echo "Axis is up (pid $AXIS2_PID)"
--- src/BeSimple/SoapClient/Tests/bin/phpwebserver.sh
+++ src/BeSimple/SoapClient/Tests/bin/phpwebserver.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 php -S localhost:8081 -t "$DIR/.." &
+PHP_PID=$!
+echo "Started process $PHP_PID"
 
 echo "Waiting until PHP webserver is ready on port 8081"
 while [[ -z `curl -s 'http://localhost:8081/' ` ]]
@@ -12,4 +14,4 @@ do
     sleep 1
 done
 
-echo "PHP webserver is up"
+echo "PHP webserver is up (pid $PHP_PID)"
